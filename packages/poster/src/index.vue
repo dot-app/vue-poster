9b07121d5ff307b5f7430f1dad77cab52e1bc92b
shaobin <lu@shaobin.cn>
2022-04-12 15:57:38 +0800

    修复ouputName

--- packages/poster/src/index.vue
+++ packages/poster/src/index.vue
@@ -22,14 +22,17 @@ export default {
     },
     methods: {
         generatorImage() {
+            let ouputName = this.ouputName || 'output.png'
+
             html2canvas(this.$refs.capture, { useCORS: true }).then(canvas => {
                 this.$refs.root.append(canvas);
                 let link = document.createElement('a');
                 link.href = canvas.toDataURL();
-                link.setAttribute('download', this.ouputName);
+                link.setAttribute('download', ouputName);
                 link.style.display = 'none';
                 document.body.appendChild(link);
                 link.click();
+                link.remove();
             })
         },
     },
